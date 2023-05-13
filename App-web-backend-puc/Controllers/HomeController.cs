cc8dcd4eb6f56c8dcc33e88aef28b214594bc282
sil-barbosa <ma.dosil82@gmail.com>
2023-05-13 16:56:06 -0300

    Adicionar arquivos de projeto.

---
+++ App-web-backend-puc/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using App_web_backend_puc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace App_web_backend_puc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
