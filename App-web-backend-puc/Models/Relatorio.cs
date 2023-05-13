813677fbb7eb38f87ed10895175597af6e7d3a54
sil-barbosa <ma.dosil82@gmail.com>
2023-05-13 17:22:44 -0300

    Featrure: Add Model Relatorio

--- App-web-backend-puc/Models/Relatorio.cs
+++ App-web-backend-puc/Models/Relatorio.cs
@@ -1,6 +1,17 @@
-﻿namespace App_web_backend_puc.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace App_web_backend_puc.Models
 {
+    [Table("Relatorios")]
     public class Relatorio
     {
+        [Key]
+        public string Produto { get; set; }
+        public string Venda { get; set; }
+        public string Estoque { get; set; }
+
+        [Required(ErrorMessage ="Obrigatório informar senha!")]
+        public int Senha{ get; set; }
     }
 }
